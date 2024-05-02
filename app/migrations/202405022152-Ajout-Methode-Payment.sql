e21a2f40cab0e0d8103291e203023ffebcd0431e
Alexandre <70542193+Alex041718@users.noreply.github.com>
2024-05-02 22:09:58 +0200

    [PRE-BDD] - Ajout-script-migration-enum
    - Ajout de script de migration sql pour les catégorie, type, genre, langues, et les méthodes de paiement (#9)
    - Ajout d'un champs de superficie en m2 dans la table _Housing
    
    Signed-off-by: Alexandre <Alexandre.alm18@gmail.com>

---
+++ app/migrations/202405022152-Ajout-Methode-Payment.sql
@@ -0,0 +1,3 @@
+INSERT INTO `_PaymentMethod` (`payMethodID`, `label`) VALUES
+    (1, 'Carte Bancaire'),
+    (2, 'Paypal');
---
+++ app/migrations/202405022156-Ajout-Categorie.sql
@@ -0,0 +1,7 @@
+INSERT INTO `_Category` (`categoryID`, `label`) VALUES
+    (7, 'Appartement'),
+    (8, 'Maison'),
+    (9, 'Villa d\'exeption'),
+(10, 'Chalet'),
+(11, 'Bateau'),
+(12, 'Logement Insolite');
