47c71c786c7722bf2e266efdf4e625aebc01c01d
Raphaelo04 <raphael.lebouedec22@gmail.com>
2024-05-14 11:37:11 +0200

    changement import police

--- app/views/consulter_detail_reservation/consulter_detail_reservation.php
+++ app/views/consulter_detail_reservation/consulter_detail_reservation.php
@@ -1,10 +1,11 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./style/ui.css">
+    <link rel="stylesheet" href="../style/ui.css">
 </head>
 <body>
     <?php
