bbc4c5b639e740c19f61ccdff0d297550d1285c1
BeastSheep <gabinou221@hotmail.com>
2024-05-15 14:00:11 +0200

    Home fix

--- app/views/components/Header/header.php
+++ app/views/components/Header/header.php
@@ -25,7 +25,7 @@
             ';
             echo $render;
 
-            require_once("./components/SearchBar/SearchBar.php");
+            require_once(__ROOT__."/views/components/SearchBar/SearchBar.php");
             SearchBar::render("search-bar search-bar--header","","./monSuperFormulaireQuiVaEtreTraiter", true);
 
             $render =  /*html*/ '
