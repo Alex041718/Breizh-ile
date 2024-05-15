3667dd7a7eccfbc2ac6b774224b7041047421f35
Raphaelo2004 <fabyangalaxy@gmail.com>
2024-05-15 13:57:59 +0200

    Consulter detail reservation (#23)
    * ajout docker-compose_iut
    
    * ajout docker-compose_iut
    
    * header corrige (ajout isScrollling)
    
    * title pixel
    
    * renommage classes
    
    ---------
    
    Co-authored-by: Raphaelo04 <raphael.lebouedec22@gmail.com>

--- app/views/consulter_detail_reservation/consulter_detail_reservation.php
+++ app/views/consulter_detail_reservation/consulter_detail_reservation.php
@@ -7,33 +7,46 @@
     <title>Document</title>
     <link rel="stylesheet" href="../style/ui.css">
     <link rel="stylesheet" href="/views/consulter_detail_reservation/consulter_detail_reservation.css">
+    <link rel="stylesheet" href="/views/components/SearchBar/SearchBar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/default.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+    <script src="./home/script.js"></script>
+    <script src="https://kit.fontawesome.com/a12680d986.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
+    <script src="./components/Helper/autocompletionHelper.js" defer></script>
+    <script src="./components/SearchBar/SearchBar.js" defer></script>
+    <link rel="stylesheet" href="/views/components/Header/header.css">
+
+    <?php // Date picker ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 </head>
 
 <?php
     require_once("../components/Header/header.php");
-    Header::render();
+    Header::render(true);
 ?>
 
 <main>
-    <div>
-        <img src="/views/assets/images/fleche.png" id="fleche" alt="fleche">
-        <h2>Ma réservation</h2>
-        <div>
+    <div class="title">
+        <div class="title__arrow">
+            <img src="/views/assets/images/fleche.png" id="fleche" alt="fleche">
+            <h2>Ma réservation</h2>
+        </div>
+        <div class="title__date">
             <h5>Voyage  à Lannion du 17/06/24 au 21/06/2024</h5>
         </div>
     </div>
-    <article>
+    <article class="informations">
         <section>
-            <div id="infos-logement">
-                <div class="image-container">
-                    <img src="/views/assets/images/house.png" alt="house">
-                </div>
-                <div class="info">
+            <div id="informations__logement">
+                <img src="/views/assets/images/house.png" alt="house">
+                <div class="informations__logement__info">
                     <h3>Perros-Guirrec - 22700</h3>
                     <p>Appartement T2</p>
                 </div>
             </div>
-            <div id="detail">
+            <div id="informations__detail">
                 <h3>Détails du prix</h3>
                 <div>
                     <p>60€ x 5 nuits x 1 occupant</p>
@@ -53,7 +66,7 @@
                 <p>360€</p>
             </div>
         </section>
-        <section id="section-2">
+        <section id="informations__section-2">
             <div>
                 
             </div>
