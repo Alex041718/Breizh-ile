1779f47c7093e17b670f965f8106cab96ceea7ff
curlyray7 <melyouns2999@gmail.com>
2024-05-15 15:14:00 +0200

    page fonctionnel

---
+++ app/views/components/Header/header.php
@@ -0,0 +1,56 @@
+
+<?php
+
+class Header {
+
+        public static function render($isScrolling = false) {
+            $render = /*html*/ '
+
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+                <link rel="stylesheet" href="/views/components/SearchBar/SearchBar.css">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/default.min.css">
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
+                <script src="https://kit.fontawesome.com/a12680d986.js" crossorigin="anonymous"></script>
+                <script src="/views/components/Helper/autocompletionHelper.js" defer></script>
+                <script src="/views/components/SearchBar/SearchBar.js" defer></script>
+                <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+                <script src="/views/components/Header/header.js"></script>
+
+                <link rel="stylesheet" href="/views/components/Header/header.css">
+
+                <header class=" '. ($isScrolling ? 'scroll' : '' ). ' header">
+                    <a class="logo" href="">
+                        <img class="logo-big" src="/views/assets/images/logo_breizh_noir.png" id="logo" alt="logo_breizh">
+                        <img class="logo-small" src="/views/assets/icons/logo.svg" alt="logo_breizh">
+                    </a>
+
+        ';
+        echo $render;
+
+            require_once(__ROOT__."/components/SearchBar/SearchBar.php");
+            SearchBar::render("search-bar search-bar--header","","./monSuperFormulaireQuiVaEtreTraiter", true);
+
+            $render =  /*html*/ '
+
+                    <div class="header__right">
+                        <i class="fa-sharp fa-regular fa-eye"></i>
+                        <i id="profil" class="fa-solid fa-user"></i>
+                    </div>
+                    <div id="options" style="display: none;">
+                        <ul>
+                            <li><a href="">Compte</a></li>
+                            <li><a href="">Mes réservations</a></li>
+                            <li><a href="">Qui sommes nous</a></li>
+                            <li><a href="">Se déconnecter</a></li>
+                        </ul>
+                    </div>
+
+            </header>
+        ';
+
+        echo $render;
+    }
+}
+
+?>
+
---
+++ web/html/client/ficheLogement/page.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fiche Logement</title>
+    <link rel="stylesheet" href="../../style/ui.css">
+    <link rel="stylesheet" href="page.css">
+
+    <script src="https://kit.fontawesome.com/a12680d986.js" crossorigin="anonymous"></script>
+
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>    
+
+</head>
+<body>  
+
+    <?php
+        require_once("../../components/Header/header.php");
+        Header::render(true);
+    ?>
+    
+    <main>
+        <div class="page">
+            <div class="photo">
+                <h2>Perros-Guirrec 22700</h2>
+                <img src="../../assets/images/12345.webp" alt="Image Logement">
+            </div>
+
+            <div class="twodiv">
+
+                <div class="details">
+                    <h4>Appartement T2</h4>
+                    <p>5 personnes • 4 chambres • 5 lits</p>
+
+                    <div class="proprio">
+                        <img src="../../assets/images/pp-test.jpg" alt="Image Proprio">
+                        <p class="para--bold">Jade Orlabit</p>
+                    </div>
+
+                    <div class="description">
+
+                        <div class="texte">
+                            <h4>Description</h4>
+                            <p class="para--18px">
+                            Idéalement situé pour découvrir la côte de granit rose : 
+                            à 10 min de Perros-Guirrec et de Beg Legue, à 15 min de trégastel, 
+                            mon appartemnce calme, vous disposerez d'une chambre (lit 2 places) et 
+                            d'un canapé lit, d'une cuisine équipée, d'une salle de bain et d'un toilette séparé...
+                            </p>
+                        </div>
+
+                        <button type="button"> <p class="para--bold">En savoir +</p></button>
+                        
+                    </div>
+
+                    <Button type="button" class="criteres"> <p>Afficher les critères</p></Button>
+
+                </div>
+
+                <div class="reservation">
+                    <h4>60 € par nuit</h4>
+
+                    <div class="preparation">
+                        <div class="datepicker">
+                            <div class="arrivee" >
+                                <p class="para--bold">Arrivée:</p>
+                                <input class="para--14px" name="startDate" id="start-date" type="date" placeholder="Ajouter une date">
+                            </div>
+
+                            <span class="vertical-line"></span>
+                            
+                            <div class="depart" >
+                                <p class="para--bold">Départ:</p>
+                                <input class="para--14px" name="endDate" id="end-date" type="date" placeholder="Ajouter une date">
+                            </div>
+                        </div>
+                        <div class="nbrClients">
+                            <button class="para--bold" id="addTravelersBtn">Ajouter des voyageurs <output id="liveTravelersCount">0</output></button>
+                            <div id="popup" class="popup">                   
+                                <div class="popup-content">
+                                    <div class="traveler-type">
+                                        <div class="adulteInfo">
+                                            <span class="para--bold">Adultes:</span>
+                                            <span>13 ans et +</span>
+                                        </div>
+                                        <div class="addbtn">
+                                            <button id="subtractAdultBtn">-</button>
+                                            <div class="nbr">
+                                                <span id="adultCount">0</span>
+                                            </div>
+                                            
+                                            <button id="addAdultBtn">+</button>
+                                        </div>
+                                    </div>
+                                
+                                    <div class="traveler-type">
+                                        <div class="enfantInfo">
+                                            <span class="para--bold">Enfants:</span>
+                                            <span>- de 12 ans</span>
+                                        </div>
+                                        <div class="addbtn">
+                                            <button id="subtractChildBtn">-</button>
+                                            <div class="nbr">
+                                                <span id="childCount">0</span>
+                                            </div>                                           
+                                            <button id="addChildBtn">+</button>
+                                        </div>
+                                    </div>
+                                    <i id="closePopupBtn" class="fa-solid fa-xmark"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="reservationBtn">
+                        <?php 
+                            require_once("../../components/Button/Button.php");
+                            Button::render("button--storybook","unId","<h4>Réserver</h4>",ButtonType::Client,true); 
+                        ?>
+                    </div>
+
+                    <div class="prix">
+                        <div class="calcul">
+                            <div><p> <u>150 € x 5 nuits</u></p></div>
+                            <div><p>750 €</p></div>
+                        </div>
+
+                        <div class="horizontal-line"></div>
+
+                        <div class="total">
+                            <div><p class="para--bold">Total</p></div>
+                            <div><p class="para--bold"> 750 €</p></div>
+                            
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="map"></div>
+    </main>
+     
+</body>
+
+<script src="page.js"></script>
+
+</html>
