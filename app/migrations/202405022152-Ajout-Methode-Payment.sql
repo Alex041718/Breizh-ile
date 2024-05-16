c63503e64f5d431293b60384e8c03cc100dabb29
DragonSlymeee <dragonslyme@protonmail.com>
2024-05-16 08:40:11 +0200

    Adaptation of the database and insertion of the test data

--- app/migrations/202405022152-Ajout-Methode-Payment.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `_PaymentMethod` (`payMethodID`, `label`) VALUES
-    (1, 'Carte Bancaire'),
-    (2, 'Paypal');
---
+++ app/migrations/202405152309-Type.sql
@@ -0,0 +1,12 @@
+INSERT INTO db._Type (label) VALUES
+('Studio'),
+('T1'),
+('T2'),
+('T3'),
+('T4'),
+('T5 et plus'),
+('F1'),
+('F2'),
+('F3'),
+('F4'),
+('F5 et plus');
