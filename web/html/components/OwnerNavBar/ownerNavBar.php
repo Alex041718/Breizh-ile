177b38b150feff04c80c64dad6a373a713207f7b
DragonSlymeee <dragonslyme@protonmail.com>
2024-05-16 16:07:03 +0200

    Save

---
+++ web/html/components/OwnerNavBar/ownerNavBar.php
@@ -0,0 +1,29 @@
+<?php
+
+class OwnerNavBar {
+    public static function render() {
+        $pages = array(
+            array("Page d'accueil", ""),
+            array("Logements", ""),
+            array("Réservations", "")
+        );
+
+        $render = /*html*/ '
+            <nav class="owner-nav-bar">
+                <ul>
+        ';
+
+        foreach ($pages as $page) {
+            $render .= '<li><a href="' . $page[1] . '">' . $page[0] . '</a></li>';
+        }
+
+        $render .= /*html*/ '
+                </ul>
+            </nav>
+        ';
+
+        echo $render;
+    }
+}
+
+?>
---
+++ web/html/owner/consulter_reservations/consulter_reservations.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultation des réservations</title>
+    <link rel="stylesheet" href="../../style/ui.css">
+    <link rel="stylesheet" href="consulter_reservations.css">
+    <link rel="stylesheet" href="../../components/OwnerNavBar/ownerNavBar.css">
+</head>
+<body>
+    <?php
+        require_once("../../components/Header/header.php");
+        require_once("../../components/OwnerNavBar/ownerNavBar.php");
+
+        Header::render(True, True);
+        OwnerNavBar::render();
+    ?>
+</body>
+</html>
