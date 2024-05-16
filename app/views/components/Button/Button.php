6552fd23184c50c9e5ca8282e652fada72057ed3
Alexandre <70542193+Alex041718@users.noreply.github.com>
2024-05-16 10:21:55 +0200

    [2.9] connexion proprio FINI (#39)
    * first commit
    
    Signed-off-by: Alexandre <Alexandre.alm18@gmail.com>
    
    * Scnd commit
    - ajout de 2 composant
    
    Signed-off-by: Alexandre <Alexandre.alm18@gmail.com>
    
    * dernier commit finition du composant Button
    
    Signed-off-by: Alexandre <Alexandre.alm18@gmail.com>
    
    * onclick sur le btn
    
    Signed-off-by: Alexandre <Alexandre.alm18@gmail.com>
    
    ---------
    
    Signed-off-by: Alexandre <Alexandre.alm18@gmail.com>

--- app/views/components/Button/Button.php
+++ app/views/components/Button/Button.php
@@ -1,15 +1,41 @@
 <?php
 
+class ButtonType {
+    const Client = 'Client';
+    const Owner = 'Owner';
+    const Delete = 'Delete';
+}
+
 class Button {
 
-    public static function render($class = "", $id = "", $text = "", $submit = false) {
+    public static function render($class = "",
+                                  $id = "",
+                                  $text = "",
+                                  $type = ButtonType::Client,
+                                  $onClick = "",
+                                  $isSecondary = true,
+                                  $submit = false) {
 
-        $buttonType = $submit ? 'submit' : 'button';
+        $isSubmit = $submit ? 'submit' : 'button';
 
+        // un switch pour gérer le type de bouton
+        $secondaryClass = $isSecondary ? '--secondary' : '';
+
+        switch ($type) {
+            case ButtonType::Client:
+                $class .= ' button--client' . $secondaryClass ;
+                break;
+            case ButtonType::Owner:
+                $class .= ' button--owner' . $secondaryClass ;
+                break;
+            case ButtonType::Delete:
+                $class .= ' button--delete' . $secondaryClass ;
+                break;
+        }
         $render = /*html*/ '
             <link rel="stylesheet" href="/views/components/Button/Button.css">
             
-            <button type="' . $buttonType . '" class="button '. $class . ' " id=" ' . $id . ' " >
+            <button type="' . $isSubmit . '" class="button '. $class . ' " id=" ' . $id . ' " onclick="' . $onClick . '">
                 ' . $text . '
             </button>
             
