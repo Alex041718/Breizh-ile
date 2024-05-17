a921c51a45b3da1f0c6b87ce88b1ef214d70d270
DragonSlyme <dragonslyme@protonmail.com>
2024-05-18 01:11:29 +0200

    Save

--- web/html/owner/consulter_reservations/consulter_reservations.php
+++ web/html/owner/consulter_reservations/consulter_reservations.php
@@ -6,27 +6,35 @@
     <title>Consultation des réservations</title>
     <link rel="stylesheet" href="../../style/ui.css">
     <link rel="stylesheet" href="consulter_reservations.css">
-    <link rel="stylesheet" href="../../components/OwnerNavBar/ownerNavBar.css">
 </head>
 <body>
     <?php
         require_once("../../components/Header/header.php");
         require_once("../../components/OwnerNavBar/ownerNavBar.php");
 
-        Header::render(True, True);
+        Header::render(isScrolling: True, isBackOffice: True);
         OwnerNavBar::render();
     ?>
     <main>
         <h3>Vos réservations</h3>
         <section class="title">
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+            <?php
+                require_once("../../components/CheckBox/CheckBox.php");
+                CheckBox::render(name: "checkbox");
+            ?>
+            <p>Date de réservation</p>
+            <p>Client</p>
+            <p>Logement</p>
+            <p>Date de départ</p>
+            <p>Date d'arrivée</p>
+            <p>Méthode de paiement</p>
+            <p>Status</p>
         </section>
     </main>
+
+    <?php
+        require_once("../../components/Footer/footer.php");
+        Footer::render(True);
+    ?>
 </body>
 </html>
