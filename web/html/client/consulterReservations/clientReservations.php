6f0b5c7c71be63cdffe3de1e923e4409cd9d8e3e
DuncanC42 <cadoret.du@gmail.com>
2024-05-21 20:50:04 +0200

    1.5 Consultation des réservations (client), mise en place d'une authentification obligatoire

--- web/html/client/consulterReservations/clientReservations.php
+++ web/html/client/consulterReservations/clientReservations.php
@@ -1,9 +1,26 @@
 <?php
+
+// Il faut tout ceci pour récupérer la session de l'utilisateur sur une page où l'on peut ne pas être connecté
+require_once '../../../models/Client.php';
+require_once '../../../services/ClientService.php';
+require_once '../../../services/SessionService.php'; // pour le menu du header
+
+
+
+// Vérification de l'authentification de l'utilisateur
+
+SessionService::system('client', '/client/reservationsListe');
+$isAuthenticated = SessionService::isClientAuthenticated();
+
+
+
 require_once '../../../services/Service.php';
 require_once '../../../services/ReservationService.php';
 require_once("../../components/ReservationCard/ReservationCard.php");
 require_once("../../components/Header/header.php");
-$clientReservationList = ReservationService::getReservationByClientId(4);
+
+$client = ClientService::GetClientById($_SESSION['user_id']);
+$clientReservationList = ReservationService::getReservationByClientId($client->getClientID());
 //sort by begin date
 usort($clientReservationList, function ($a, $b) {
     return $a->getBeginDate() <=> $b->getBeginDate();
@@ -17,7 +34,7 @@ usort($clientReservationList, function ($a, $b) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../style/ui.css">
-    <link rel="stylesheet" href="clientReservations.css">
+    <link rel="stylesheet" href="/client/consulterReservations/clientReservations.css">
     <script src="../../components/ReservationCard/ReservationCard.js"></script>
 <!--    <script src="clientReservations.js"></script>-->
     <title>Reservation History</title>
@@ -26,7 +43,7 @@ usort($clientReservationList, function ($a, $b) {
 header
 <?php
 require_once("../../components/Header/header.php");
-Header::render(true);
+Header::render(true,false, $isAuthenticated);
 ?>
 <body>
 <main>
