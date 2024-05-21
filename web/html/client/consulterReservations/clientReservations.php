6219ac58bb417829b7d3f8e83447bffd591df094
shi2gami42 <122746523+DuncanC42@users.noreply.github.com>
2024-05-21 21:16:53 +0200

    [1.5] consultation des reservations
    * 1.5 Consultation des réservations (client), Fix des liens
    
    * 1.5 Consultation des réservations (client), style de la page, redirection vers la page de reservation correspondant à l'item cliqué et mise en place d'une image par défaut
    
    * 1.5 Consultation des réservations (client), mise en place d'une authentification obligatoire
    
    * petite modif
    
    ---------
    
    Co-authored-by: Alexandre <Alexandre.alm18@gmail.com>

---
+++ web/html/client/consulterReservations/clientReservations.php
@@ -0,0 +1,71 @@
+<?php
+
+// Il faut tout ceci pour récupérer la session de l'utilisateur sur une page où l'on peut ne pas être connecté
+require_once '../../../models/Client.php';
+require_once '../../../services/ClientService.php';
+require_once '../../../services/SessionService.php'; // pour le menu du header
+
+
+
+// Vérification de l'authentification de l'utilisateur
+
+SessionService::system('client', '/client/client/reservations-liste');
+$isAuthenticated = SessionService::isClientAuthenticated();
+
+
+
+require_once '../../../services/Service.php';
+require_once '../../../services/ReservationService.php';
+require_once("../../components/ReservationCard/ReservationCard.php");
+require_once("../../components/Header/header.php");
+
+$client = ClientService::GetClientById($_SESSION['user_id']);
+$clientReservationList = ReservationService::getReservationByClientId($client->getClientID());
+//sort by begin date
+usort($clientReservationList, function ($a, $b) {
+    return $a->getBeginDate() <=> $b->getBeginDate();
+});
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../style/ui.css">
+    <link rel="stylesheet" href="/client/consulterReservations/clientReservations.css">
+    <script src="/components/ReservationCard/ReservationCard.js"></script>
+<!--    <script src="clientReservations.js"></script>-->
+    <title>Vos réservation</title>
+
+</head>
+<?php
+require_once("../../components/Header/header.php");
+Header::render(true,false, $isAuthenticated);
+?>
+<body>
+<main>
+    <div class="title">
+        <i class="fa-regular fa-less-than"></i>
+        <h2 class="title__text">Vos réservations</h2>
+    </div>
+    <div class="reservation-list">
+        <div class="reservation-list__container">
+            <?php foreach ($clientReservationList as $index => $reservationItem) : ?>
+                <div class="reservation-list__container__card" data-index="<?php echo $index; ?>">
+<!--                    -->
+                    <?php ReservationCard::render("reservation-list__container__card__render", "", $reservationItem); ?>
+<!--                    </a>-->
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</main>
+<?php
+require_once("../../components/Footer/footer.php");
+Footer::render();
+?>
+</body>
+</html>
+
