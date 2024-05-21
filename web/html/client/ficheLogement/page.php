5d73f5882d40a35f7d869a58883e7b526ea7f3b2
curlyray7 <87017606+curlyray7@users.noreply.github.com>
2024-05-21 10:55:34 +0200

    FIX Date Picker Dans Fiche lodgement
    * css valide
    
    * Resolution du porbleme de css
    
    * ajoute du cursor:pointer aux buttons
    
    * resolution du porbleme de taille datepicker + cursor
    
    * Accessibilite (#88)
    
    * accessibilite taille et maj detail resa
    
    * reglage ReservationHandler apres pull (connard jules)
    
    * reglage conflits raph jules (#90)
    
    * reglage bug jules raph sur les deux pages (#91)
    
    * resolution du porbleme de taille de la classe .preparation
    
    * page fonctionnelle sauf map
    
    * correction du datepicker
    
    ---------
    
    Co-authored-by: Raphaelo2004 <fabyangalaxy@gmail.com>

--- web/html/client/ficheLogement/page.php
+++ web/html/client/ficheLogement/page.php
@@ -7,10 +7,13 @@
     <link rel="stylesheet" href="../../style/ui.css">
     <link rel="stylesheet" href="/client/ficheLogement/page.css">
     <script src="https://kit.fontawesome.com/a12680d986.js" crossorigin="anonymous"></script>
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    
     <script src="/client/ficheLogement/page.js"></script>
 </head>
 <body>
