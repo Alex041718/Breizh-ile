3bdc911c0e8fac47da64c064ec941c8c1e7dd382
Alexandre <Alexandre.alm18@gmail.com>
2024-05-22 06:54:31 +0200

    fist

--- web/html/client/bid/bid.php
+++ web/html/client/bid/bid.php
@@ -18,10 +18,7 @@ $isAuthenticated = SessionService::isClientAuthenticated();
 
     <link rel="stylesheet" href="../../style/ui.css">
     <script src="https://kit.fontawesome.com/a12680d986.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <link rel="stylesheet" href="/client/bid/bid.css">
 </head>
 <body>
 <?php
@@ -30,9 +27,47 @@ Header::render(true,false,$isAuthenticated,$_SERVER['REQUEST_URI']);
 ?>
 
 <main class="bid">
-    <h2 class="bid__title-page">Devis de la location</h2>
+    <span>la barre de progression</span>
     <div class="bid__container">
 
+        <h2 class="bid__title-page">Votre devis pour :</h2>
+
+        <div class="bid__container__info">
+            <div class="bid__container__info__info-detail">
+
+            </div>
+            <div class="bid__container__info__pay-recap">
+
+                <div class="bid__container__info__pay-recap__price-detail">
+                    <h3>Détails du prix</h3>
+                    <div>
+                        <p class="para--18px"><?= "100 exel" ?> € x <?= "3" ?> nuits x <?= "8"  ?> occupant(s)</p>
+                        <p class="para--18px"><?= "500" ?> €</p>
+                    </div>
+                    <div>
+                        <p class="para--18px" >Frais de service</p>
+                        <p class="para--18px"><?= "20" ?> €</p>
+                    </div>
+                    <div>
+                        <p class="para--18px">Taxee de séjour</p>
+                        <p class="para--18px"><?= "32" ?> €</p>
+                    </div>
+                </div>
+                <hr>
+                <div class="informations__left__total">
+                    <div>
+                        <h3>Total TTC</h3>
+                        <p class="para--18px"><?= "800"?> €</p>
+                    </div>
+                </div>
+
+                <?php require_once('../../components/Button/Button.php');
+                Button::render("Payer","id","Procéder au paiement",ButtonType::Client,"alert('Payer')",false);
+                ?>
+
+
+            </div>
+        </div>
     </div>
 </main>
 
