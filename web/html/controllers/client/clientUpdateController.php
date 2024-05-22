98aacaba4f6ef6ca6e8fb2f9496a54d84cfde372
KimHazard <kim.hazard13@gmail.com>
2024-05-22 12:34:54 +0200

    ajout de la modifications du profile

---
+++ web/html/controllers/client/clientUpdateController.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once '../../../models/Client.php';
+require_once '../../../services/ClientService.php';
+
+// Traitement du formulaire de mise à jour
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $lastname = $_POST['lastname'];
+    $firstname = $_POST['firstname'];
+    $nickname = $_POST['nickname'];
+    $mail = $_POST['mail'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $address = $_POST['address'];
+    $gender = $_POST['genderID'];
+    $birthDate = $_POST['birthDate'];
+    $creationDate = $_POST['creationDate'];
+    $clientID = $_POST['clientID'];
+
+    $client = ClientService::GetClientById($clientID);
+
+    try {
+        // Mettre à jour les informations du client
+        $client->setLastname($lastname);
+        $client->setFirstname($firstname);
+        $client->setNickname($nickname);
+        $client->setMail($mail);
+        $client->setPhoneNumber($phoneNumber);
+        $client->getAddress()->setPostalAddress($address);
+        $client->getGender()->setLabel($gender);
+        $client->setBirthDate(new DateTime($birthDate));
+        $client->setCreationDate(new DateTime($creationDate));
+
+        // Modifier le client dans la base de données
+        ClientService::ModifyClient($client);
+
+        // Rediriger ou afficher un message de succès
+        redirect('/client/profil?success=1');
+    } catch (Exception $e) {
+        // Gérer les erreurs (par exemple, afficher un message d'erreur à l'utilisateur)
+        $error = $e->getMessage();
+    }
+}
+
+?>
--- web/services/ClientService.php
+++ web/services/ClientService.php
@@ -81,6 +81,85 @@ class ClientService extends Service
 
         return new Client($row['clientID'], $row['isBlocked'], $row['mail'], $row['firstname'], $row['lastname'], $row['nickname'], $row['password'], $row['phoneNumber'], new DateTime($row['birthDate']), $row['consent'], new DateTime($row['lastConnection']), new DateTime($row['creationDate']), $image, $gender, $address);
     }
+
+    public static function ModifyClient(Client $client): bool
+{
+    $existingClient = self::GetClientById($client->getClientID());
+    
+    if (!$existingClient) {
+        throw new Exception('Client not found');
+    }
+    
+    // Modifie l'image si elle a été modifiée
+    if ($client->getImage() !== $existingClient->getImage()) {
+        $image = ImageService::CreateImage($client->getImage());
+        $client->setImage($image);
+    }
+    
+    // Modifie l'adresse si elle a été modifiée
+    if ($client->getAddress() !== $existingClient->getAddress()) {
+        $address = AddressService::CreateAddress($client->getAddress());
+        $client->setAddress($address);
+    }
+
+    // Modifie le genre si il a été modifié
+    if ($client->getGender() !== $existingClient->getGender()) {
+        $gender = GenderService::GetGenderById($client->getGender()->getGenderID());
+        $client->setGender($gender);
+    }
+
+    // Modifier la table
+    $pdo = self::getPDO();
+    $stmt = $pdo->prepare('UPDATE _User SET 
+        mail = :mail, 
+        firstname = :firstname, 
+        lastname = :lastname, 
+        nickname = :nickname, 
+        password = :password, 
+        phoneNumber = :phoneNumber, 
+        birthDate = :birthDate, 
+        consent = :consent, 
+        lastConnection = :lastConnection, 
+        creationDate = :creationDate, 
+        imageID = :imageID, 
+        genderID = :genderID, 
+        addressID = :addressID 
+        WHERE userID = :userID');
+
+    $success = $stmt->execute(array(
+        'mail' => $client->getMail(),
+        'firstname' => $client->getFirstname(),
+        'lastname' => $client->getLastname(),
+        'nickname' => $client->getNickname(),
+        'password' => $client->getPassword(),
+        'phoneNumber' => $client->getPhoneNumber(),
+        'birthDate' => $client->getBirthDate()->format('Y-m-d H:i:s'),
+        'consent' => $client->getConsent(),
+        'lastConnection' => $client->getLastConnection()->format('Y-m-d H:i:s'),
+        'creationDate' => $client->getCreationDate()->format('Y-m-d H:i:s'),
+        'imageID' => $client->getImage()->getImageID(),
+        'genderID' => $client->getGender()->getGenderID(),
+        'addressID' => $client->getAddress()->getAddressID(),
+        'userID' => $client->getClientID()
+    ));
+
+    if (!$success) {
+        throw new Exception('Failed to modify client');
+    }
+
+    // Modifier si le client est bloqué
+    if ($client->getIsBlocked() !== $existingClient->getIsBlocked()) {
+        $stmt = $pdo->prepare('UPDATE _Client SET isBlocked = :isBlocked WHERE clientID = :clientID');
+        $stmt->execute(array(
+            'isBlocked' => $client->getIsBlocked(),
+            'clientID' => $client->getClientID()
+        ));
+    }
+
+    return true;
+}
+
+
 }
 
 ?>
