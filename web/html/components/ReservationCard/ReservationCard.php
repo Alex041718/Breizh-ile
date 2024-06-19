cb03006bd2621cfe8d8b613a4b00f5624c4909f3
DuncanC42 <cadoret.du@gmail.com>
2024-06-19 10:23:17 +0200

    FIX - style de la page ClientReservations et du composant ReservationCard

--- web/html/components/ReservationCard/ReservationCard.php
+++ web/html/components/ReservationCard/ReservationCard.php
@@ -12,6 +12,7 @@ class ReservationCard
         $imageSrc = $housing->getImage()->getImageSrc();
         $reservation_nbJours =  ReservationService::getNbJoursReservation($reservation->getBeginDate(), $reservation->getEndDate());
         $prixTTC = $housing->getPriceIncl() * $reservation_nbJours + $reservation->getServiceCharge() + $reservation->getTouristTax();
+        $prixTTC = number_format($prixTTC, 2, ',', ' ');
         $render =  /*html*/
             '
             <script src="https://kit.fontawesome.com/a12680d986.js" crossorigin="anonymous"></script>
@@ -20,31 +21,31 @@ class ReservationCard
                 <div class="reservation-card__img-container">
                     <img src=" ' . $imageSrc . '" alt="" onerror=this.src="/FILES/images/default-house-image.png">
                 </div>
-                <div class="reservation-card__data-container">
-                    <h4 class="reservation-card__data-container__title" title="' . $housing->getTitle() . '">' . $housing->getTitle() . '</h4>
-                    <div class="reservation-card__data-container__description">
-                        ' . $housing->getShortDesc() . '
-                    </div>
-                    <div class="reservation-card__data-container__down">
-                        <div class="reservation-card__data-container__down__ville">
-                            <i class="fa-solid fa-location-dot"></i>
-                            <div>' . $housing->getAddress()->getPostalCode(). ' - '. $housing->getAddress()->getCity() .'</div>
+                <div class="reservation-card__content">
+                    <div class="reservation-card__content__data-container">
+                        <h4 class="reservation-card__content__data-container__title" title="' . $housing->getTitle() . '">' . $housing->getTitle() . '</h4>
+                        <div class="reservation-card__content__data-container__description">
+                            ' . $housing->getShortDesc() . '
                         </div>
-                        <div class="reservation-card__data-container__down__personne">
-                            <i class="fa-solid fa-user"></i>
-                            <div>' . $data->getNbPerson() . '</div>
+                        <div class="reservation-card__content__data-container__down">
+                            <div class="reservation-card__content__data-container__down__ville">
+                                <i class="fa-solid fa-location-dot"></i>
+                                <div>' . $housing->getAddress()->getPostalCode(). ' - '. $housing->getAddress()->getCity() .'</div>
+                            </div>
+                            <div class="reservation-card__content__data-container__down__personne">
+                                <i class="fa-solid fa-user"></i>
+                                <div>' . $data->getNbPerson() . '</div>
+                            </div>
                         </div>
                     </div>
-                    
+                    <div class="reservation-card__content__right">
+                        <div class="reservation-card__content__right__prix"> ' . $prixTTC  . '€ TTC</div>
+                        <div class="reservation-card__content__right__date">' . $reservation->getBeginDate()->format('d/m/Y') . '</div>
+                    </div>
                 </div>
-                <div class="reservation-card__right">
-                    <div class="reservation-card__right__prix"> ' . $prixTTC  . '€ TTC</div>
-                    <div class="reservation-card__right__date">' . $reservation->getBeginDate()->format('d/m/Y') . '</div>
-                </div>
-                
             </div>
-        
         ';
+
 
         echo $render;
     }
